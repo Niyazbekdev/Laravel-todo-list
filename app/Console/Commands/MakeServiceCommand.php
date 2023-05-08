07983b012ce8e1aad28dbf68fa9cbff45a66248e
Niyazbekdev <niyazbekk001@gmail.com>
2023-05-08 10:51:28 +0500

    telescope + make Command(services) + Service(abstract class)

---
+++ app/Console/Commands/MakeServiceCommand.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Console\GeneratorCommand;
+
+class MakeServiceCommand extends GeneratorCommand
+{
+    protected $signature = 'make:service {name}';
+    protected $type = 'Service';
+    protected $description = 'Create a new Service class';
+
+    protected function getStub(): string
+    {
+        return __DIR__.'/../../../stubs/service.stub';
+    }
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace . '\Services';
+    }
+}
