5f40c9f7f83e0753c2baf65c837edbf3bfe0eea5
Niyazbekdev <niyazbekk001@gmail.com>
2023-05-25 23:36:48 +0500

    api route

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    use JsonResponseController;
+    //
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,22 @@
 <?php
 
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['guest'])->group(function(){
+    Route::post('/SignUp', [UserController::class, 'register']);
+    Route::post('/SignIn', [UserController::class, 'login']);
 });
+Route::middleware('auth:sanctum')
+    ->prefix('/tasks')
+    ->group(function(){
+        Route::get('/', [TaskController::class, 'index']);
+        Route::get('/{id}', [TaskController::class, 'show']);
+        Route::post('/', [TaskController::class, 'store']);
+        Route::put('/{id}', [TaskController::class, 'update']);
+        Route::patch('/{id}', [TaskController::class, 'edit']);
+        Route::delete('/{id}', [TaskController::class, 'destroy']);
+    });
+
